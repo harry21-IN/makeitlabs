b742d0f5d7c908f2e3150b046098228c85bef8be
Harry <91789772+harry21-IN@users.noreply.github.com>
2024-10-01 18:30:35 +0530

    Add files via upload

---
+++ src/components/Title.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Title.css";
+
+function Title() {
+  return (
+    <div className="Title">
+        <div className="title-content">
+          <span>YOU NEED IT ?</span>
+          <span>
+            LET'S <span>Make!t</span>
+          </span>
+        </div>
+        <div className="title-desc">
+          <p>
+            Transform your ideas into tangible creations with our high-quality
+            3D printing devices
+          </p>
+        </div>
+        <div className="title-btn">
+          <button>Make!t</button>
+        </div>
+    </div>
+  );
+}
+
+export default Title;
